67299aa8898259ac369f003d2a01b979ccabb497
Riffle-1024 <liuyalu1993@163.com>
2021-09-29 13:37:48 +0800

    测试

---
+++ RippleView.h
@@ -0,0 +1,18 @@
+//
+//  RippleView.h
+//  MyProject
+//
+//  Created by mxchip on 2021/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RippleView : UIView
+
+@property (nonatomic,assign) CGFloat multiple;
+
+@end
+
+NS_ASSUME_NONNULL_END
